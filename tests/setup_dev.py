49ed0c42366498b5502c14ed5706ea2b9817ed3d
Joris van Zundert <joris.van.zundert@gmail.com>
2017-01-30 22:34:43 +0100

    Improved test setup, added requirements

--- tests/setup_dev.py
+++ tests/setup_dev.py
@@ -1,4 +1,4 @@
 import sys
 import os
-path_to_sfsf = os.path.abspath( os.path.join( os.path.dirname( os.path.abspath( __file__ ) ), os.pardir, 'sfsf' ) )
+path_to_sfsf = os.path.abspath( os.path.join( os.path.dirname( os.path.abspath( __file__ ) ), os.pardir ) )
 sys.path.append( path_to_sfsf )
