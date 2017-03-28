8b07e5b625a3ff34c738485873334a501e8c29b0
Joris van Zundert <joris.van.zundert@gmail.com>
2017-03-28 15:41:35 +0200

    Bug fixes for field tests

--- scr_create_training_data.py
+++ scr_create_training_data.py
@@ -4,6 +4,6 @@ import pickle
 
 # main
 training_factory = training_data_factory.TrainingDataFactory()
-# training_data = training_factory.create_by_indices( 'wpg_data.csv', (1,52,-600,-549), sample_size=5000, source=sfsf_config.TXT  )
-training_data = training_factory.create_by_indices( 'wpg_data.csv', (1,5,-600,-596), sample_size=5000, source=sfsf_config.TXT  )
+#training_data = training_factory.create_by_indices( 'wpg_data.csv', (1,5,-600,-596), sample_size=5000, source=sfsf_config.TXT  )
+training_data = training_factory.create_by_indices( 'wpg_data.csv', (1,51,-599,-549), sample_size=5000, source=sfsf_config.TXT  )
 pickle.dump( training_data, open( 'sfsf_training_data.pickle', 'wb' ) )
--- scr_predict.py
+++ scr_predict.py
@@ -3,6 +3,7 @@ from sfsf import training_data_factory
 from sfsf import deep_learning_model
 from datetime import datetime
 import numpy
+import csv
 import pickle
 
 def report( report_row, csv_writer ):
@@ -14,13 +15,14 @@ def report( report_row, csv_writer ):
 training_factory = training_data_factory.TrainingDataFactory()
 training_data = pickle.load( open( 'sfsf_training_data.pickle', 'rb' ) )
 vectorizer = training_data[ 'vectorizer' ]
-testing_top, testing_bottom = training_factory.get_top_bottom_by_indices( 'wpg_data.csv', (53,58,-608,-603) )
+#testing_top, testing_bottom = training_factory.get_top_bottom_by_indices( 'wpg_data.csv', (53,54,-608,-607) )
+testing_top, testing_bottom = training_factory.get_top_bottom_by_indices( 'wpg_data.csv', (53,73,-540,-520) )
 testing_data_top = training_factory.sample_txts( testing_top, 5000 )
 testing_data_bottom = training_factory.sample_txts( testing_bottom, 5000 )
-isbns_top = [ tuple[0] for tuple in testing_data_top for sample in tuple[1]
-isbns_bottom = [ tuple[0] for tuple in testing_data_bottom for sample in tuple[1]
-testing_tdm_top = vectorizer.transform( [ sample for tuple in testing_data_top for sample in tuple[1] )
-testing_tdm_bottom = vectorizer.transform( [ sample for tuple in testing_data_bottom for sample in tuple[1] )
+isbns_top = [ tuple[0] for tuple in testing_data_top for sample in tuple[1] ]
+isbns_bottom = [ tuple[0] for tuple in testing_data_bottom for sample in tuple[1] ]
+testing_tdm_top = vectorizer.transform( [ sample for tuple in testing_data_top for sample in tuple[1] ] )
+testing_tdm_bottom = vectorizer.transform( [ sample for tuple in testing_data_bottom for sample in tuple[1] ] )
 model = deep_learning_model.DeepLearningModel()
 # training_data, batch_size, epochs
 accuracy = model.build( ( training_data['x'], training_data['y'] ), 10, 5 )
@@ -32,7 +34,7 @@ for idx, prediction in enumerate( predictions ):
     predictions_top.append( ( isbns_top[idx], prediction[0] ) )
 predictions = model.predict( numpy.array( testing_tdm_bottom.toarray() ) )
 for idx, prediction in enumerate( predictions ):
-    predictions_bottom.append( ( isbns_top[idx], prediction[0] ) )
+    predictions_bottom.append( ( isbns_bottom[idx], prediction[0] ) )
 
 # reporting
 report_file_name = 'report_{d}.csv'.format( d=datetime.now().strftime( '%Y%m%d_%H%M' ) )
@@ -43,32 +45,36 @@ isbn_data = training_factory.get_isbn_data( 'wpg_data.csv' )
 headers = ['deep_learning_data_type', 'NUR', 'ISBN', 'title', 'author', 'total sold', 'prediction' ]
 csv_writer.writerow( headers )
 print( '\t'.join( headers ) )
+training_isbns_reported = []
 # combine training data y and isbns
 for idx, item in enumerate( training_data['y'] ):
-    report_row = []
-    # 1 is top, 0 is flop
-    if item == 1:
-        report_row.append( 'training_top' )
-    else:
-        report_row.append( 'training_bottom' )
-    #isbn: find the data
-    isbn_info = next( isbn_inf for isbn_inf in isbn_data if isbn_data[1]==training_data['isbns'][idx] )
-    report_row.append( isbn_info )
-    report_row.append( 'NA' )
-    report( report_row, csv_writer )
+    if not training_data['isbns'][idx] in training_isbns_reported: 
+        report_row = []
+        # 1 is top, 0 is flop
+        if item == 1:
+            report_row.append( 'training_top' )
+        else:
+            report_row.append( 'training_bottom' )
+        #isbn: find the data
+        isbn_info = next( isbn_inf for isbn_inf in isbn_data if isbn_inf[1]==training_data['isbns'][idx] )
+        report_row.extend( isbn_info )
+        report_row.append( 'NA' )
+        report( report_row, csv_writer )
+        training_isbns_reported.append( training_data['isbns'][idx] )
 for prediction in predictions_top:
     report_row = []
     report_row.append( 'testing_top' )
     #isbn: find the data
-    isbn_info = next( isbn_inf for isbn_inf in isbn_data if isbn_data[1]==prediction[0] )
-    report_row.append( isbn_info )
-    report_row.append( prediction[1] )
+    isbn_info = next( isbn_inf for isbn_inf in isbn_data if isbn_inf[1]==prediction[0] )
+    report_row.extend( isbn_info )
+    report_row.append( str( prediction[1] ) )
     report( report_row, csv_writer )
 for prediction in predictions_bottom:
     report_row = []
     report_row.append( 'testing_bottom' )
     #isbn: find the data
-    isbn_info = next( isbn_inf for isbn_inf in isbn_data if isbn_data[1]==prediction[0] )
-    report_row.append( isbn_info )
-    report_row.append( prediction[1] )
+    isbn_info = next( isbn_inf for isbn_inf in isbn_data if isbn_inf[1]==prediction[0] )
+    report_row.extend( isbn_info )
+    report_row.append( str( prediction[1] ) )
     report( report_row, csv_writer )
+report_file.close()
